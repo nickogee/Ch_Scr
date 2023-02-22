58a599309fdc28958a8eafe9704d4e72d99b243d
nickogee <nick.ogee@gmail.com>
2023-02-22 10:56:01 +0600

    22_02_2023

--- glovo_fetchs.py
+++ glovo_fetchs.py
@@ -1,6 +1,6 @@
 
 
-PARAMS =  {
+PARAMS = {
   "headers": {
     "accept": "application/json",
     "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -10,19 +10,15 @@ PARAMS =  {
     "glovo-app-type": "customer",
     "glovo-app-version": "7",
     "glovo-device-id": "1289182602",
-    "glovo-dynamic-session-id": "d9671303-52c7-4df0-884f-b4a26861d566",
+    "glovo-dynamic-session-id": "ff9ce6de-a396-4382-bf04-b9e76701e2d0",
     "glovo-language-code": "ru",
     "glovo-location-city-code": "ALA",
-    "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+    "sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"macOS\"",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "x-datadog-origin": "rum",
-    "x-datadog-parent-id": "6610788034335120495",
-    "x-datadog-sampling-priority": "1",
-    "x-datadog-trace-id": "8401224261977427342"
+    "sec-fetch-site": "same-site"
   },
   "referrerPolicy": "same-origin",
   "body": None,
@@ -31,11 +27,13 @@ PARAMS =  {
 
 URL_SERV = "https://api.glovoapp.com/v3/"
 
-# URL_FST = "https://api.glovoapp.com/v3/stores/309810/addresses/465610/content?nodeType=DEEP_LINK&link=lichnaya-gigiena-sc.217688990"
+# URL_FST = "https://api.glovoapp.com/v3/stores/309810/addresses/465610/content?nodeType=DEEP_LINK&link=sladosti-i-sneki-sc.224280930"
 
 # Фрукты Овощи:
 URL_FST = "https://api.glovoapp.com/v3/stores/309810/addresses/465610/content?nodeType=DEEP_LINK&link=ovoshchi-frukty-i-yagody-sc.217688976"
 
-# CATEGORY = 'Личная гигиена'
+# CATEGORY = 'Сладости и Снеки'
 CATEGORY = 'Овощи, Фрукты и Ягоды'
 ###############
+
+
--- kaspi.py
+++ kaspi.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -6,7 +5,7 @@ from selenium.webdriver.chrome.service import Service
 import time
 import random
 import datetime
-from share_functions import make_DataFrame, upload_to_excel, disc, disc_prercent
+from share_functions import make_DataFrame, upload_to_excel
 
 
 CATEGORY = 'Овощи, фрукты, ягоды, грибы'
@@ -16,6 +15,11 @@ URL = 'https://kaspi.kz/shop/c/food/?q=%3AallMerchants%3AMagnum%3AavailableInZon
 
 CHROMEDRIVER = '/Users/hachimantaro/PycharmProjects/Ch_scrp/chromedriver/chromedriver'
 
+'''
+window.cdc_adoQpoasnfa76pfcZLmcfl_Array
+window.cdc_adoQpoasnfa76pfcZLmcfl_Promise
+window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol
+'''
 
 class KaspiParser():
 
@@ -29,11 +33,24 @@ class KaspiParser():
 
     def make_driver(self):
         useragent = UserAgent()
-        driver = webdriver.Chrome(service=Service(CHROMEDRIVER))
 
         options = webdriver.ChromeOptions()
         options.add_argument(f'user-agent={useragent.chrome}')
+        options.add_argument('--disable-blink-features=AutomationControlled')
         options.add_experimental_option("excludeSwitches", ['enable-automation'])
+        options.add_experimental_option('useAutomationExtension', False)
+
+        driver = webdriver.Chrome(service=Service(CHROMEDRIVER), options=options)
+        driver.maximize_window()
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            'source': '''
+                delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array
+                delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise
+                delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol
+            '''
+        })
+
+
         self.options = options
         self.driver = driver
 
@@ -49,6 +66,7 @@ class KaspiParser():
     def search_data(self):
 
         self.driver.get(URL)
+        time.sleep(100)
         self.rand_pause()
         Almaty = self.__find_element(By.CSS_SELECTOR, '[data-city-id="750000000"]')
         Almaty.click()
@@ -156,109 +174,6 @@ def make_driver():
     options.add_argument(f'user-agent={useragent.chrome}')
     options.add_experimental_option("excludeSwitches", ['enable-automation'])
     return driver
-
-
-def search_data(driver):
-    rezult = []
-    driver.get(URL)
-    time.sleep(5 + random.randint(0, 7))
-    Almaty = driver.find_element(By.CSS_SELECTOR, '[data-city-id="750000000"]')
-    Almaty.click()
-
-    category_list = driver.find_elements(By.CLASS_NAME, 'tree__link')
-
-    for category in category_list:
-
-        # Найдена нужная категория
-        if category.text == CATEGORY:
-            category.click()
-            time.sleep(5 + random.randint(-2, 5))
-
-            # ищем список подкатегорий
-            sub_category_list = driver.find_elements(By.CLASS_NAME, 'tree__link')
-
-            # начиная с индекса 3 - это список подкатегорий
-            for i in range(3, len(sub_category_list)):
-
-                if i > 3:
-                    # ищем список подкатегорий занова, после каждой следующей итерации
-                    sub_category_list = driver.find_elements(By.CLASS_NAME, 'tree__link')
-
-                sub_category = sub_category_list[i]
-                sub_category_text = sub_category.text
-                sub_category.click()
-                time.sleep(5 + random.randint(0, 15))
-
-                next_page = True
-                while next_page:
-                    #  Внутри категории ищем элементы с названием и ценой продуктов
-                    product_list = driver.find_elements(By.CLASS_NAME, 'item-card__info')
-
-                    for product_card in product_list:
-                        title_tag = product_card.find_element(By.CLASS_NAME, 'item-card__name')
-                        price_tag = product_card.find_element(By.CLASS_NAME, 'item-card__prices-price')
-                        priceL = [j for j in price_tag.text if j.isdigit()]
-                        l = {
-                            'title': title_tag.text,
-                            'sub_category': sub_category_text,
-                            'category': CATEGORY,
-                            'brand': '',
-                            'priceActual': float(''.join(priceL)),
-                            'pricePrevious': 0,
-                            }
-
-                        rezult.append(l)
-
-                    next_page = push_the_button_next(driver)
-
-                else:
-                    # ищем список элементов с категориями и подкатегориями
-                    #  и возвращаемся на страницу со списком подкатегорий (кликаем по основной категории)
-                    sub_category_list_ = driver.find_elements(By.CLASS_NAME, 'tree__link')
-                    category_ = sub_category_list_[2]
-                    category_.click()
-                    time.sleep(5 + random.randint(0, 10))
-
-            else:
-                # все данные по искомой категории просмотренны
-                break
-
-    return rezult
-
-
-def push_the_button_next(driver):
-
-
-    try:
-        next_btn_list = driver.find_elements(By.CLASS_NAME, 'pagination__el')
-        next_btn = next_btn_list[-1]
-    except Exception:
-        # нет кнопок с переключением на след. страницу - это единственная страница по текущей подкатегории
-        return False
-
-    try:
-        disabled_btn = driver.find_element(By.CLASS_NAME, '_disabled')
-    except Exception:
-        disabled_btn = None
-
-    if disabled_btn and disabled_btn == next_btn:
-        return False
-    else:
-        next_btn.click()
-        time.sleep(5 + random.randint(-2, 10))
-        return True
-
-
-def to_file(rezult):
-    now = datetime.datetime.now()
-    df = pd.DataFrame(rezult)
-
-    df['discount'] = df.loc[:, ['pricePrevious', 'priceActual']].apply(disc, axis=1)
-    df['discount_prc'] = df.loc[:, ['pricePrevious', 'discount']].apply(disc_prercent, axis=1)
-
-    df.to_excel(f'data_rezult/KaspiMagnum - {CATEGORY} {now.strftime("%d_%m_%Y")}.xlsx')
-    return
-
 
 
 def main():
